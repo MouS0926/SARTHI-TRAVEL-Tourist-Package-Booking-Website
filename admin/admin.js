3bc355c85796c801a94d8da322091f5cf5bf60d3
Moumita <moupriya2609@gmail.com>
2023-05-09 16:44:11 +0530

    test

--- admin/admin.js
+++ admin/admin.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
-  fetchProduct();
+    fetchProduct();
 });
 
 let cardsContainer = document.getElementById("data-list-wrapper");
@@ -9,97 +9,96 @@ const searchInput = document.querySelector("#search");
 const editInput = document.querySelector("#edit");
 
 async function fetchProduct() {
-  try {
-    const response = await fetch("http://localhost:9971/destinations");
-    const users = await response.json();
-    renderCards(users);
-    return users;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await fetch("http://localhost:9971/destinations");
+        const users = await response.json();
+        renderCards(users);
+        return users;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 function renderCards(users) {
-  console.log(users);
-  cardsContainer.innerHTML = "";
+    console.log(users);
+    cardsContainer.innerHTML = "";
 
-  users.forEach((user) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
+    users.forEach((user) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-    const boxImg = document.createElement("div");
-    boxImg.classList.add("boxImg");
-    const image = document.createElement("img");
-    image.src = `${user.image}`;
-    image.alt = `${user.name}'s profile picture`;
-    boxImg.appendChild(image);
-    card.appendChild(boxImg);
+        const boxImg = document.createElement("div");
+        boxImg.classList.add("boxImg");
+        const image = document.createElement("img");
+        image.src = `${user.image}`;
+        image.alt = `${user.name}'s profile picture`;
+        boxImg.appendChild(image);
+        card.appendChild(boxImg);
 
-    const boxdetails = document.createElement("div");
-    boxdetails.classList.add("boxDetails");
-    const name = document.createElement("h3");
-    name.textContent = user.name;
-    boxdetails.appendChild(name);
+        const boxdetails = document.createElement("div");
+        boxdetails.classList.add("boxDetails");
+        const name = document.createElement("h3");
+        name.textContent = user.name;
+        boxdetails.appendChild(name);
 
-    const location = document.createElement("p");
-    location.textContent = `${user.location}`;
-    boxdetails.appendChild(location);
+        const location = document.createElement("p");
+        location.textContent = `${user.location}`;
+        boxdetails.appendChild(location);
 
-    const price = document.createElement("p");
-    price.textContent = `Price: ₹${user.price * 10}`;
-    boxdetails.appendChild(price);
+        const price = document.createElement("p");
+        price.textContent = `Price: ₹${user.price * 10}`;
+        boxdetails.appendChild(price);
 
-    const btnDiv = document.createElement("div");
-    const deleteIcon = document.createElement("button");
-    deleteIcon.classList.add("deleteIcon");
-    deleteIcon.textContent = "Delete";
-    deleteIcon.addEventListener("click", async () => {
-      let del_id = user.id;
-      try {
-        const response = await fetch(
-          `http://localhost:9971/destinations/${del_id}`,
-          {
-            method: "DELETE",
-          }
-        );
-        if (response.ok) {
-          card.remove(); //this will remove the card
-        }
-      } catch (error) {
-        console.error(error);
-      }
-      fetchUsers();
-    });
-    btnDiv.appendChild(deleteIcon);
-
-    const editIcon = document.createElement("button");
-    const aTag = document.createElement("a");
-    aTag.setAttribute("href", "#");
-    aTag.setAttribute("data-id", user.id);
-    aTag.classList.add("cardLink");
-    aTag.innerText = "Edit";
-
-    editIcon.addEventListener("click", function (e) {
-      e.preventDefault();
-      const editLink = document.querySelectorAll(".cardLink");
-      console.log(editLink);
-      editLink.forEach((elementbtn) => {
-        elementbtn.addEventListener("click", (event) => {
-          event.preventDefault();
-          let id = event.target.dataset.id;
-          console.log(id);
-          updateAllPopulateProduct(id);
+        const btnDiv = document.createElement("div");
+        const deleteIcon = document.createElement("button");
+        deleteIcon.classList.add("deleteIcon");
+        deleteIcon.textContent = "Delete";
+        deleteIcon.addEventListener("click", async() => {
+            let del_id = user.id;
+            try {
+                const response = await fetch(
+                    `http://localhost:9971/destinations/${del_id}`, {
+                        method: "DELETE",
+                    }
+                );
+                if (response.ok) {
+                    card.remove(); //this will remove the card
+                }
+            } catch (error) {
+                console.error(error);
+            }
+            fetchUsers();
         });
-      });
+        btnDiv.appendChild(deleteIcon);
+
+        const editIcon = document.createElement("button");
+        const aTag = document.createElement("a");
+        aTag.setAttribute("href", "#");
+        aTag.setAttribute("data-id", user.id);
+        aTag.classList.add("cardLink");
+        aTag.innerText = "Edit";
+
+        editIcon.addEventListener("click", function(e) {
+            e.preventDefault();
+            const editLink = document.querySelectorAll(".cardLink");
+            console.log(editLink);
+            editLink.forEach((elementbtn) => {
+                elementbtn.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    let id = event.target.dataset.id;
+                    console.log(id);
+                    updateAllPopulateProduct(id);
+                });
+            });
+        });
+        editIcon.appendChild(aTag);
+
+        btnDiv.appendChild(editIcon);
+        boxdetails.appendChild(btnDiv);
+        card.appendChild(boxdetails);
+
+        cardsContainer.appendChild(card);
     });
-    editIcon.appendChild(aTag);
-
-    btnDiv.appendChild(editIcon);
-    boxdetails.appendChild(btnDiv);
-    card.appendChild(boxdetails);
-
-    cardsContainer.appendChild(card);
-  });
 }
 
 //  add products
@@ -110,125 +109,125 @@ let destinationPriceInput = document.getElementById("product-price");
 let destinationCreateBtn = document.getElementById("add-product");
 
 destinationCreateBtn.addEventListener("click", () => {
-  let destinationName = destinationNameInput.value;
-  let destinationImg = destinationImgInput.value;
-  let destinationLocation = destinationLocationInput.value;
-  let destinationPrice = destinationPriceInput.value;
+    let destinationName = destinationNameInput.value;
+    let destinationImg = destinationImgInput.value;
+    let destinationLocation = destinationLocationInput.value;
+    let destinationPrice = destinationPriceInput.value;
 
-  let newEmpObj = {
-    name: destinationName,
-    image: destinationImg,
-    location: destinationLocation,
-    price: destinationPrice,
-  };
+    let newEmpObj = {
+        name: destinationName,
+        image: destinationImg,
+        location: destinationLocation,
+        price: destinationPrice,
+    };
 
-  console.log(newEmpObj);
-  fetch("http://localhost:9971/destinations", {
-    method: "POST",
-    body: JSON.stringify(newEmpObj),
-    headers: {
-      "Content-type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      fetchProduct(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    console.log(newEmpObj);
+    fetch("http://localhost:9971/destinations", {
+            method: "POST",
+            body: JSON.stringify(newEmpObj),
+            headers: {
+                "Content-type": "application/json",
+            },
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            fetchProduct(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 //  update products
 let updateDestinationNameInput = document.getElementById("update-product-name");
 let updateDestinationImgInput = document.getElementById("update-product-image");
 let updateDestinationBrandInput = document.getElementById(
-  "update-product-brand"
+    "update-product-brand"
 );
 let updateDestinationPriceInput = document.getElementById(
-  "update-product-price"
+    "update-product-price"
 );
 let updateDestinationCreateBtn = document.getElementById("update-product");
 
 function updateAllPopulateProduct(changeid) {
-  console.log(changeid);
-  fetch(`http://localhost:9971/destinations/${changeid}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      // Populate the input fields with the extracted values
-      updateDestinationNameInput.value = data.name;
-      updateDestinationImgInput.value = data.image;
-      updateDestinationBrandInput.value = data.location;
-      updateDestinationPriceInput.value = data.price;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    console.log(changeid);
+    fetch(`http://localhost:9971/destinations/${changeid}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            // Populate the input fields with the extracted values
+            updateDestinationNameInput.value = data.name;
+            updateDestinationImgInput.value = data.image;
+            updateDestinationBrandInput.value = data.location;
+            updateDestinationPriceInput.value = data.price;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 // function changeUpdatesProducts() {
-updateDestinationCreateBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  let updateproductName = updateDestinationNameInput.value;
-  let updateproductImg = updateDestinationImgInput.value;
-  let updateproductBrand = updateDestinationBrandInput.value;
-  let updateproductPrice = updateDestinationPriceInput.value;
+updateDestinationCreateBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    let updateproductName = updateDestinationNameInput.value;
+    let updateproductImg = updateDestinationImgInput.value;
+    let updateproductBrand = updateDestinationBrandInput.value;
+    let updateproductPrice = updateDestinationPriceInput.value;
 
-  let newUpdatedProductObj = {
-    name: updateproductName,
-    image: updateproductImg,
-    location: updateproductBrand,
-    price: updateproductPrice,
-  };
+    let newUpdatedProductObj = {
+        name: updateproductName,
+        image: updateproductImg,
+        location: updateproductBrand,
+        price: updateproductPrice,
+    };
 
-  fetch(`http://localhost:9971/destinations/${changeid}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newUpdatedProductObj),
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      fetchProduct(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    fetch(`http://localhost:9971/destinations/${changeid}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newUpdatedProductObj),
+        })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            fetchProduct(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 });
 
 let sortProductLowtoHigh = document.getElementById("sort-low-to-high");
-sortProductLowtoHigh.addEventListener("click", function () {
-  fetch(`http://localhost:9971/destinations/?_sort=price,views&_order=asc`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((productData) => {
-      console.log(productData);
-      renderCards(productData);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+sortProductLowtoHigh.addEventListener("click", function() {
+    fetch(`http://localhost:9971/destinations/?_sort=price,views&_order=asc`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((productData) => {
+            console.log(productData);
+            renderCards(productData);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 });
 
 let sortProductHightoLow = document.getElementById("sort-high-to-low");
 sortProductHightoLow.addEventListener("click", () => {
-  console.log("low");
-  fetch(`http://localhost:9971/destinations?_sort=price,views&_order=desc`)
-    .then((res) => res.json())
-    .then((productData) => {
-      renderCards(productData);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    console.log("low");
+    fetch(`http://localhost:9971/destinations?_sort=price,views&_order=desc`)
+        .then((res) => res.json())
+        .then((productData) => {
+            renderCards(productData);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 // filterSelect.addEventListener("change", function (e) {
@@ -255,30 +254,30 @@ sortProductHightoLow.addEventListener("click", () => {
 //     searchQuery;
 //   }
 // }
-searchInput.addEventListener("input", function (e) {
-  const searchQuery = e.target.value.toLowerCase().trim();
+searchInput.addEventListener("input", function(e) {
+    const searchQuery = e.target.value.toLowerCase().trim();
 
-  fetch("http://localhost:9971/destinations")
-    .then((response) => {
-      return response.json();
-    })
-    .then((users) => {
-      const filteredUsers = searchUsersByName(users, searchQuery);
-      renderCards(filteredUsers);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    fetch("http://localhost:9971/destinations")
+        .then((response) => {
+            return response.json();
+        })
+        .then((users) => {
+            const filteredUsers = searchUsersByName(users, searchQuery);
+            renderCards(filteredUsers);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 });
 
 function searchUsersByName(users, searchQuery) {
-  if (searchQuery === "") {
-    return users;
-  } else {
-    return users.filter(
-      (user) =>
-        user.name.toLowerCase().includes(searchQuery) ||
-        user.location.toLowerCase().includes(searchQuery)
-    );
-  }
+    if (searchQuery === "") {
+        return users;
+    } else {
+        return users.filter(
+            (user) =>
+            user.name.toLowerCase().includes(searchQuery) ||
+            user.location.toLowerCase().includes(searchQuery)
+        );
+    }
 }
